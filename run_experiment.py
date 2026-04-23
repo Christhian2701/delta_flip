96eb5f50e25fbb4859c9a2f35769353ba8627971
christhian <christhian.studies@gmail.com>
2026-04-23 15:23:05 -0300

    feat: changes for testing alternative cnn architectures

--- run_experiment.py
+++ run_experiment.py
@@ -123,8 +123,8 @@ def main(args):
     
     # Define algorithms to run
     # Comparison: FLIPS vs FedAvg vs FedBuff vs FedProx vs FedLama
-    algorithms = ['flips', 'fedavg', 'fedbuff', 'fedprox', 'fedlama']
-    #algorithms = ['flips']
+    #algorithms = ['flips', 'fedavg', 'fedbuff', 'fedprox', 'fedlama']
+    algorithms = ['fedprox']
     
     for algo in algorithms:
         print(f"\n" + "#" * 60)

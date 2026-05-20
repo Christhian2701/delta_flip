778a6a4a20faa404248292861094ae10e4090d59
Christhian2701 <christhian.studies@gmail.com>
2026-05-20 11:59:16 +0000

    feat: new results under deeper cnn

--- run_experiment.py
+++ run_experiment.py
@@ -126,8 +126,8 @@ def main(args):
     
     # Define algorithms to run
     # Comparison: FLIPS vs FedAvg vs FedBuff vs FedProx vs FedLama
-    #algorithms = ['flips', 'fedavg', 'fedbuff', 'fedprox', 'fedlama']
-    algorithms = ['flips', 'fedavg']
+    algorithms = ['flips', 'fedavg', 'fedbuff', 'fedprox', 'fedlama']
+    #algorithms = ['flips', 'fedavg']
     
     for algo in algorithms:
         print(f"\n" + "#" * 60)
